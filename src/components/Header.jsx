ae06de4a8215d364b9511d5f1ad67795dc8d9151
Mighty <realofficialvictor@gmail.com>
2025-10-22 15:59:49 +0100

    header nav link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,6 +23,8 @@ export function Header() {
               <li><a href="#home">Home</a></li>
               <li><a href="#about">About Us</a></li>
               <li><a href="#admission">Admission</a></li>
+              <li><a href="#portal">E-Portal</a></li>
+              <li><a href="#gallery">Gallery</a></li>
               <li><a href="#contact">Contact</a></li>
             </ul>
           </nav>
