aacc0817bdf1fdd260910c749fc8591df47f215c
Mighty/ <realofficialvictor@gmail.com>
2025-10-23 13:44:26 +0100

    Fix import case for Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Header } from './components/header'
+import { Header } from './components/Header'
 import { Carousel } from './components/Carousel'
 import { AboutUsFlip } from './components/About'
 import { FourColumnComponent } from './components/Four'
