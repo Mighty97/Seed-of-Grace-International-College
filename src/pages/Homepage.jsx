430990af593de5b9341afd5272e40460f405fb9d
Mighty <realofficialvictor@gmail.com>
2025-10-25 16:03:03 +0100

    homepage carousel and aboutus component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,13 @@
 import { Header } from '../components/Header'
+import { Carousel } from '../components/Carousel'
+import { AboutUsFlip } from '../components/About';
 
 export function HomePage() {
   return(
     <>
       <Header />
+      <Carousel />
+      <AboutUsFlip />
     </>
   );
 }
