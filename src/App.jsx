ee3cbcbd5aa708cf0c6d815b623fdf258869dcf7
Mighty <realofficialvictor@gmail.com>
2025-10-25 15:34:16 +0100

    homepage building

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { Header } from './components/header'
-import { Carousel } from './components/Carousel'
-import { AboutUsFlip } from './components/About'
-import { FourColumnComponent } from './components/Four'
+
 import './App.css'
 
 function App() {
  return (
   <>
-    <Header />
-    <Carousel />
-    <AboutUsFlip />
-    <FourColumnComponent />
+ 
   </>
  )
 }
